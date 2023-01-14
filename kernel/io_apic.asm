eff976d6629bc2a84353f626968224edbac43fbd
slowy07 <slowy.arfy@gmail.com>
2023-01-15 00:37:29 +0700

    feat: check io
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- kernel/io_apic.asm
+++ kernel/io_apic.asm
@@ -1,29 +1,32 @@
 kernel_io_apic_connect:
-    push rax
-    push rbx
-    push rdi
+	; preserve original registers
+	push	rax
+	push	rbx
+	push	rdi
 
-    ; kernel environment variable / routines base address
-    mov rdi, qword [kernel_environment_base_address]
-    mov rdi, qword [rdi + KERNEL_STRUCTURE.io_apic_base_address]
+	; kernel environment variables/rountines base address
+	mov	rdi,	qword [kernel_environment_base_address]
+	mov	rdi,	qword [rdi + KERNEL_STRUCTURE.io_apic_base_address]
 
-    ; inside lower half of register
-    add ebx, KERNEL_IO_APIC_iowin_low
-    mov qword [rdi + KERNEL_IO_APIC_ioregsel], ebx
+	; inside lower half of register
+	add	ebx,	KERNEL_IO_APIC_iowin_low
+	mov	dword [rdi + KERNEL_IO_APIC_ioregsel],	ebx
 
-    ; save lower half of interrupt vector
-    mov dword [rdi + KERNEL_IO_APIC_iowin], eax
+	; save lower half of interrupt vector
+	mov	dword [rdi + KERNEL_IO_APIC_iowin],	eax
 
-    ; inside higher half
-    add ebx, KERNEL_IO_APIC_iowin_high - KERNEL_IO_APIC_iowin_low
-    mov dword [rdi + KERNEL_IO_APIC_ioregsel], ebx
+	; inside higher half
+	add	ebx,	KERNEL_IO_APIC_iowin_high - KERNEL_IO_APIC_iowin_low
+	mov	dword [rdi + KERNEL_IO_APIC_ioregsel],	ebx
 
-    ; save higher of interrupt vector
-    shr rax, STATIC_MOVE_HIGH_TO_EAX_shift
-    mov dword [rdi + KERNEL_IO_APIC_iowin], eax
+	; save higher half of interrupt vector
+	shr	rax,	STATIC_MOVE_HIGH_TO_EAX_shift
+	mov	dword [rdi + KERNEL_IO_APIC_iowin],	eax
 
-    pop rdi
-    pop rbx
-    pop rax
+	; restore original registers
+	pop	rdi
+	pop	rbx
+	pop	rax
 
-    ret
+	; return from routine
+	ret
