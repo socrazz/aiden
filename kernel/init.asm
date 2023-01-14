3080a5402baa5a38c53896a80ee19b7a8edb724b
slowy07 <slowy.arfy@gmail.com>
2023-01-14 09:49:14 +0700

    fix: idt configuration
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- kernel/init.asm
+++ kernel/init.asm
@@ -1,129 +1,143 @@
+; structures, definitions
 ; global
-%include "default.inc"
-; lib
-%include "library/elf.inc"
-%include "library/pkg.inc"
-%include "library/sys.inc"
+%include	"default.inc"
+; library
+%include	"library/elf.inc"
+%include	"library/pkg.inc"
+%include	"library/sys.inc"
 ; driver
-%include "kernel/driver/ps2.inc"
-%include "kernel/driver/serial.inc"
+%include	"kernel/driver/ps2.inc"
+%include	"kernel/driver/rtc.inc"
+%include	"kernel/driver/serial.inc"
 ; kernel
-%include "kernel/config.inc"
-%include "kernel/exec.inc"
-%include "kernel/gdt.inc"
-%include "kernel/idt.inc"
-%include "kernel/io_apic.inc"
-%include "kernel/lapic.inc"
-%include "kernel/page.inc"
-%include "kernel/storage.inc"
-%include "kernel/task.inc"
+%include	"kernel/config.inc"
+%include	"kernel/exec.inc"
+%include	"kernel/gdt.inc"
+%include	"kernel/idt.inc"
+%include	"kernel/io_apic.inc"
+%include	"kernel/ipc.inc"
+%include	"kernel/lapic.inc"
+%include	"kernel/library.inc"
+%include	"kernel/page.inc"
+%include	"kernel/storage.inc"
+%include	"kernel/task.inc"
 ; kernel environment initialization routines
-%include "kernel/init/acpi.inc"
-%include "kernel/init/ap.inc"
-%include "kernel/init/limine.inc"
+%include	"kernel/init/acpi.inc"
+%include	"kernel/init/ap.inc"
+%include	"kernel/init/limine.inc"
 
 ; 64 bit code
 [bits 64]
 
-default rel
+; we are using Position Independed Code
+default	rel
 
-; initialization procedure of kernel environment
-global init
+; main initialization procedure of kernel environment
+global	init
 
 ; information for linker
-section .data
-    ; variable, and constant
-    %include "kernel/data.asm"
-    %include "kernel/init/data.asm"
+section	.data
+	; variables, constants
+	%include	"kernel/data.asm"
+	%include	"kernel/init/data.asm"
 
-; information more about linker
+; information for linker
 section .text
-    ; including libraries
-    %include "library/pkg.asm"
-    %include "library/elf.asm"
-    %include "library/string/compare.asm"
+	; routines
 
-    ; driver
-    %include "kernel/driver/ps2.asm"
-    %include "kernel/driver/serial.asm"
-    
-    ; kernel env
-    %include "kernel/exec.asm"
-    %include "kernel/idt.asm"
-    %include "kernel/io_apic.asm"
-    %include "kernel/lapic.asm"
-    %include "kernel/library.asm"
-    %include "kernel/memory.asm"
-    %include "kernel/page.asm"
-    %include "kernel/service.asm"
-    %include "kernel/storage.asm"
-    %include "kernel/syscall.asm"
-    %include "kernel/task.asm"
-
-    ; kernel environment initialize routine
-    %include "kernel/init/acpi.asm"
-	%include "kernel/init/ap.asm"
-	%include "kernel/init/exec.asm"
-	%include "kernel/init/framebuffer.asm"
-	%include "kernel/init/free.asm"
-	%include "kernel/init/gdt.asm"
-	%include "kernel/init/idt.asm"
-	%include "kernel/init/library.asm"
-	%include "kernel/init/memory.asm"
-	%include "kernel/init/page.asm"
-	%include "kernel/init/smp.asm"
-	%include "kernel/init/storage.asm"
-	%include "kernel/init/task.asm"
-
+	; library
+	%include	"library/pkg.asm"
+	%include	"library/elf.asm"
+	%include	"library/string/compare.asm"
+	%include	"library/string/length.asm"
+	; drivers
+	%include	"kernel/driver/ps2.asm"
+	%include	"kernel/driver/rtc.asm"
+	%include	"kernel/driver/serial.asm"
+	; kernel
+	%include	"kernel/exec.asm"
+	%include	"kernel/idt.asm"
+	%include	"kernel/io_apic.asm"
+	%include	"kernel/lapic.asm"
+	%include	"kernel/library.asm"
+	%include	"kernel/memory.asm"
+	%include	"kernel/page.asm"
+	%include	"kernel/service.asm"
+	%include	"kernel/storage.asm"
+	%include	"kernel/syscall.asm"
+	%include	"kernel/task.asm"
+	; kernel environment initialization routines
+	%include	"kernel/init/acpi.asm"
+	%include	"kernel/init/ap.asm"
+	%include	"kernel/init/exec.asm"
+	%include	"kernel/init/framebuffer.asm"
+	%include	"kernel/init/free.asm"
+	%include	"kernel/init/gdt.asm"
+	%include	"kernel/init/idt.asm"
+	%include	"kernel/init/ipc.asm"
+	%include	"kernel/init/library.asm"
+	%include	"kernel/init/memory.asm"
+	%include	"kernel/init/page.asm"
+	%include	"kernel/init/smp.asm"
+	%include	"kernel/init/storage.asm"
+	%include	"kernel/init/task.asm"
+; void
 init:
-    ; configure failover output
-    call driver_serial_string
-    
-    ; show kernel name, version, architecture and build time
-    mov rsi, kernel_log_welcome
-    call driver_serial_string
-    
-    ; create binary memory map
-    call kernel_init_memory
+	; configure failover output
+	call	driver_serial
 
-    ; store information about framebuffer properties
-    call kernel_init_framebuffer
+	; show kernel name, version, architecture and build time
+	mov	rsi,	kernel_log_welcome
+	call	driver_serial_string
 
-    ; parse ACPI tables
-    call kernel_init_acpi
+	; create binary memory map
+	call	kernel_init_memory
 
-    ; recreate kernel paging structure
-    call kernel_init_page
+	; store information about framebuffer properties
+	call	kernel_init_framebuffer
 
-    ; switch to new kernel paging array
-    mov rax, ~KERNEL_PAGE_mirror
-    and rax, qword [r8 + KERNEL_STRUCTURE.page_base_addres]
-    mov cr3, rax
+	; parse ACPI tables
+	call	kernel_init_acpi
 
-    ; set new stack pointer
-    mov rsp, KERNEL_TASK_pointer
+	; recreate kernel's paging structures
+	call	kernel_init_page
 
-    ; create global descriptor table
-    call kernel_init_gdt
+	; switch to new kernel paging array
+	mov	rax,	~KERNEL_PAGE_mirror	; physical address
+	and	rax,	qword [r8 + KERNEL_STRUCTURE.page_base_address]
+	mov	cr3,	rax
 
-    ; create interrupt descriptor table
-    call kernel_init_idt
+	; set new stack pointer
+	mov	rsp,	KERNEL_TASK_STACK_pointer
 
-    ; create task queue
-    call kernel_init_task
+	; create Global Descriptor Table
+	call	kernel_init_gdt
 
-    ; initialize PS2 keyboward mouse driver
-    call driver_ps2
+	; create Interrupt Descriptor Table
+	call	kernel_init_idt
 
-    ; register all available data carriers
-    call kernel_init_storage
+	; create Task queue
+	call	kernel_init_task
 
-    ; execute library subsytem
-    call kernel_init_library
-    
-    ; execute init process
-    call kernel_init_exeic
+	; configure Real Time Clock
+	call	driver_rtc
 
-    ; check initialize test other cpu
-    jmp kernel_init_smp
-    
+	; initialize PS2 keyboard/mouse driver
+	call	driver_ps2
+
+	; create interprocess communication system
+	call	kernel_init_ipc
+
+	; register all available data carriers
+	call	kernel_init_storage
+
+	; prepare library subsystem
+	call	kernel_init_library
+
+	; execute init process
+	call	kernel_init_exec
+
+	; below, initialization functions does not guarantee original registers preservation
+
+	; initialize other CPUs
+	jmp	kernel_init_smp
